5ae89826313a706bb5c1e70576a6c8b94a0750ed
anilzown <anilkumar_ga@yahoo.com>
2016-11-18 12:37:46 -0500

    app seperation

--- public/app/view/views.js
+++ public/app/view/views.js
@@ -1,16 +1,17 @@
 angular.module('website', ['ngRoute']).
-    config(function ($routeProvider) {
-        $routeProvider.
-            when('/about', {templateUrl: 'partials/about.html'}).
-            when('/experiments', {templateUrl: 'partials/experiments.html'}).
-            when('/home', {templateUrl: 'partials/new.html'}).
-            when('/tasklist', {templateUrl: 'partials/tasklist.html'}).
-            when('/taskdetail', {templateUrl: 'partials/taskdetail.html'}).
-            when('/login', {templateUrl: 'partials/login.html'}).
-              when('/googleLogin', {templateUrl: 'partials/googleLogin.html'}).
-               when('/location', {templateUrl: 'partials/location.html'}).
-              when('/googleSignOut', {templateUrl: 'partials/googleSignOut.html'}).
-              
-            otherwise({redirectTo: '/home'});
-    })
-    
+config(function($routeProvider) {
+    $routeProvider.
+        // when('/about', {templateUrl: 'partials/about.html'}).
+        // when('/experiments', {templateUrl: 'partials/experiments.html'}).
+        // when('/home', {templateUrl: 'partials/new.html'}).
+        // when('/tasklist', {templateUrl: 'partials/tasklist.html'}).
+        // when('/taskdetail', {templateUrl: 'partials/taskdetail.html'}).
+    when('/login', { templateUrl: 'partials/ca-login.html' }).
+    when('/profile', { templateUrl: 'partials/ca-userprofile.html' }).
+    when('/commutetracker', { templateUrl: 'partials/ca-commutetracker.html' }).
+        // when('/googleLogin', {templateUrl: 'partials/googleLogin.html'}).
+    when('/location', { templateUrl: 'partials/location.html' }).
+        // when('/googleSignOut', {templateUrl: 'partials/googleSignOut.html'}).
+
+    otherwise({ redirectTo: '/login' });
+})
